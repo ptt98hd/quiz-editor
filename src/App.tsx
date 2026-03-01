0c61ca2b8f2e22102db118ac6a96a646140e0ed0
ptt98hd <thanhpham.hd.98@gmail.com>
2026-03-01 10:35:38 +0700

    feat: inital project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { router } from '@/routes';
+import { RouterProvider } from 'react-router';
+import './App.css';
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter, type RouteObject } from 'react-router';
+
+const routes: RouteObject[] = [];
+
+export const router = createBrowserRouter(routes);
